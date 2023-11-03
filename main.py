90688a24067236f2a3e66c5fb37b6a96c5c2d9e2
anrulev <anrulev@gmail.com>
2023-11-03 20:08:45 +0300

    ##

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import datetime
 from pprint import pprint
-from config import open_weather_token
+from config1 import open_weather_token
 
 
 def get_weather(city, open_weather_token):
--- main_weather_tg_bot.py
+++ main_weather_tg_bot.py
@@ -1,6 +1,6 @@
 import requests
 import datetime
-from config import tg_bot_token, open_weather_token
+from config1 import tg_bot_token, open_weather_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
